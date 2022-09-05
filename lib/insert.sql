587c96ee1e2687a345f033b687bf85fd01326504
Jaden Lund <lundjaden@gmail.com>
2022-09-05 17:59:16 -0400

    commit tree 69b07e86570037371a51373adffac30ca07a649f

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+ INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 1, 'M', 'black', 'mean', 1);
+ INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'white', 'nice', 1);
+ INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'M', 'brown', 'hungry', 1);
+ INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'F', 'brown', 'happy', 1);
+ INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 23, 'F', 'brown', 'sad', 0);
+ INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 23, 'M', 'brown', 'mean', 0);
+ INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 23, 'F', 'black', 'nice', 1);
+ INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 21, 'M', 'white', 'greedy', 1);
